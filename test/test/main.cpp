0c724f8029866b682ab4bcd52e059d3777d4a03f
KlogeBukser <imvacant0923@gmail.com>
2022-08-25 00:14:25 +0200

    test

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
+#include <cmath>
+
+
+double u(double x);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     std::cout << "Hello, World!\n";
+    
+    double result[100] = {0};
+    
+    for (int i=0;i<100;i++) {
+        double x = double(i)/100;
+        result[i] = u(x);
+        std::cout << result[i];
+    }
+    
     return 0;
 }
+
+double u(double x) {
+    return 1 - (1 - exp(-10*x))*x - exp(-10*x);
+}
+
+
