3b1d9ed843c1ed9810784c7af7c41e9c12c2fa0c
KlogeBukser <imvacant0923@gmail.com>
2022-08-24 23:20:57 +0200

    test

---
+++ test/test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Bukser Kloge on 24/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
