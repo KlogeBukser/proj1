77be9a5e846d5e80a4db11937d1516a9c064c828
torstbje <torstbje@uio.no>
2022-08-31 16:31:10 +0200

    changed pointer arrays to vectors

--- TriDiagonalSolver.cpp
+++ TriDiagonalSolver.cpp
@@ -1,9 +1,11 @@
 #include "triDiagonalSolver.h"
 #include <iostream>
+using namespace std;
 
-
-void general(double *ap, double *bp, double *cp, double *gp, double *vp, int n){
+vector<double> general(vector<double> a, vector<double> b, vector<double> c, vector<double> g){
+  int n = b.size();
   double p;
+  vector<double> v(n);
   c[0] = c[0]/b[0]; g[0] = g[0]/b[0];
   for (int i = 1; i < n-1; n++){
     p = 1/(b[i] -a[i]*c[i-1]);
@@ -13,5 +15,5 @@ void general(double *ap, double *bp, double *cp, double *gp, double *vp, int n){
   for (int i = n-2; i > -1; i--){
     v[i] = g[i] - v[i+1]*c[i];
   }
-  return;
+  return v;
 }
