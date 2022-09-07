b98dfd21f80e8568281b5b213eca2255c458d343
torstbje <torstbje@uio.no>
2022-09-07 17:08:34 +0200

    can makes several plots at the same time

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ from plot_from_file import read_and_plot
 
 os.system("g++ -o main.out main.cpp -std=c++17")
 
-os.system("./main.out")
-read_and_plot(["exact.txt","general.txt"],"Solutions to Poisson's equation")
+n = 101
+os.system("./main.out " + str(n + 1))
+read_and_plot(["exact.txt","general.txt"],"Solutions to Poisson's equation for n = " + str(n))
+
+"""
+for n in (10,100,1000,10000):
+ os.system("./main.out " + str(n + 1))
+ read_and_plot(["exact.txt","general.txt"],"Solutions to Poisson's equation for n = " + str(n))
+"""
