252a994703ed97fb962949a9a1ce0416b276ab7d
KlogeBukser <imvacant0923@gmail.com>
2022-09-09 14:17:10 +0200

    renamed functions in calError and added relative error calculations

--- calError.cpp
+++ calError.cpp
@@ -12,17 +12,23 @@ using namespace std;
 
 // TODO: calculate u(x) - v_x without boundary points
 
-vector<double> logAbsError(vector<double> u, vector<double> v, int step) {
+vector<double> logError(vector<double> u, vector<double> v, int step, int is_abs) {
     
     vector<double> error(step);
     error[0] = 0.0;
     error[step-1] = 0.0;
     
-    for (int i=1;i<step;i++) { // starting from i=1 to avoid boundary points as error is 0 at those points.
-        error[i] = log10(abs(u[i]-v[i]));
-        
+    if (is_abs) {
+        for (int i=1;i<step;i++) { // starting from i=1 to avoid boundary points as error is 0 at those points.
+            error[i] = log10(abs(u[i]-v[i]));
+            
+        }
+    }
+    else {
+        for (int i=1;i<step;i++) { // starting from i=1 to avoid boundary points as error is 0 at those points.
+            error[i] = log10(abs(u[i]-v[i])/u[i]);
+        }
     }
     return error;
 }
-
 
