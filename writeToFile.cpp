6a74434146066de320fe1aafbe26aa12779c9292
KlogeBukser <imvacant0923@gmail.com>
2022-09-09 13:14:09 +0200

    fixed indentation problems

--- writeToFile.cpp
+++ writeToFile.cpp
@@ -6,34 +6,37 @@ double u(double y) {
     return 1 - (1 - exp(-10))*y - exp(-10*y);
 }
 
-void writeExact(int n){
+vector<double> writeExact(int n){
   /*Takes filename, number of steps,
   and a function to write two vectors to file without using vector elements*/
-  double result[n];
-  double x = 0;
-  double h = double(1)/(n - 1);
-  std::ofstream output;
-  output.open("textfiles/exact.txt", std::ios::out);
-  if(!output)
+    double x = 0;
+    double h = double(1)/(n - 1);
+    vector<double> exa_sol(n);
+    std::ofstream output;
+    output.open("textfiles/exact.txt", std::ios::out);
+    if(!output)
       {
           std::cerr<<"Cannot open the output file."<<std::endl;
       }
-  for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
       output << std::setiosflags(std::ios::scientific) << x << "," << u(x) << std::endl;
       x += h;
-  }
-  output.close();
+      exa_sol[i] = u(x);
+    }
+
+    output.close();
+    return exa_sol;
 }
 
 void writeFile(string filename, vector<double> vectors[], int nVectors){
   /*This version takes any number of vectors and write them all to file*/
-  ofstream outFile;
-  outFile.open(filename);
-  vector<double> vec;
+    ofstream outFile;
+    outFile.open(filename);
+    vector<double> vec;
 
-  unsigned long n = vectors[0].size();
+    unsigned long n = vectors[0].size();
 
-  for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){
     //loops through each element in the vectors
     for (int j = 0; j < nVectors; j++){
       //loops through set of vectors
@@ -43,7 +46,7 @@ void writeFile(string filename, vector<double> vectors[], int nVectors){
         outFile << ",";
       }
     }
-    outFile << std::endl;
+        outFile << std::endl;
   }
-  outFile.close();
+    outFile.close();
 }
